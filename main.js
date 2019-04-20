28f472e653f34dc7585853f2294aee516f7f11a6
yee <sverreabo@gmail.com>
2019-04-20 17:12:10 +0200

    Ferdig for i dag

---
+++ main.js
@@ -0,0 +1,12 @@
+function main() {
+	document.getElementById("currenttime").innerHTML = Date();
+	if (navigator.onLine) {
+	setTimeout("main()", 500);
+	} else {
+		alert("You are offline");
+		main();
+	}
+}
+
+console.log("Successfully started main.js");
+main();
